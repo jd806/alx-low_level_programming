ac1588ef9339da27e36b2a49ca9469ae425d0fdf
John Doe <doe80647@gmail.com>
2022-03-10 14:12:18 -0800

    Last stuff

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,24 @@
-#include <stdio.h>                                                                                                                                                                              
-                                                                                                                                                                                                
-/**                                                                                                                                                                                             
- * main - Entry point                                                                                                                                                                           
- *                                                                                                                                                                                              
- * Return: Always 0 (Success)                                                                                                                                                                   
- */                                                                                                                                                                                             
-                                                                                                                                                                                                
-int main(void)                                                                                                                                                                                  
-{                                                                                                                                                                                               
-        char acharacter;                                                                                                                                                                        
-        int ainteger;                                                                                                                                                                           
-        long along;                                                                                                                                                                             
-        long long alonglong;                                                                                                                                                                    
-        float afloat;                                                                                                                                                                           
-                                                                                                                                                                                                
-        printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));                                                                                                                            
-        printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));                                                                                                                              
-        printf("Size of a long int: %lu byte(s)\n", sizeof(along));                                                                                                                             
-        printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));                                                                                                                    
-        printf("Size of a float: %lu byte(s)\n", sizeof(afloat));                                                                                                                               
-                                                                                                                                                                                                
-        return (0);                                                                                                                                                                             
-}  
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a_char;
+	int an_int;
+	long a_long;
+	long long l_long;
+	float a_float;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(a_char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(an_int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(a_long));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(l_long));
+	printf("Size of a float: %lu byte(s)\n", sizeof(a_float));
+
+	return (0);
+}
