516d03cad3b5eaccb356cfaaece2b891e24fd265
wizlif <wizlif.144@gmail.com>
2022-03-21 21:45:21 +0300

    feat: strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,11 +10,13 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-int i;
+int i = 0;
 
-for (i = 0; src[i] != '\0'; i++)
-dest[i] = src[i];
-for ( ; i < src[i]; i++)
-dest[i] = '\0';
+while (*(src + i))
+{
+*(dest + i) = *(src + i);
+i++;
+}
+*(dest + i) = '\0';
 return (dest);
 }
